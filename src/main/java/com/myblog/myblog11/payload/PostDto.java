452dc1cb55a4b896eb99f6781d04f5240751f13a
ashutoshsinha <ashusinha7890070@gmail.com>
2024-02-01 23:05:49 +0530

    127: Create Save Post Feature Entity class created on 2nd-Feb

---
+++ src/main/java/com/myblog/myblog11/payload/PostDto.java
@@ -0,0 +1,20 @@
+package com.myblog.myblog11.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+
+    private long id;
+
+    private String title;
+    private String description;
+    private String content;
+
+
+}
